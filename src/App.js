f95725fb731c545556df7271557a08a2e89a9ab9
SergeiKazanin <serega7667@gmail.com>
2023-03-16 16:30:31 +0700

    Added pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { Link } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import MyProjects from "./pages/MyProjects"; 
+import AboutMe from "./pages/AboutMe"
+import AcademicSvg from "./components/svg/AcademicSvg";
+import HomeSvg from "./components/svg/HomeSvg";
+import AboutMeSvg from "./components/svg/AboutMeSvg";
+
 
 function App() {
   return (
@@ -8,9 +14,26 @@ function App() {
       <header className="fixed w-full h-12 top-0 left-0 z-50 shadow-md">
         <div className="px-4 h-full max-w-screen-lg mx-auto">
           <nav className="flex items-center gap-4 h-full ">
-            <a href="/" className="font-greatvibes transition-colors hover:text-indigo-500 text-2xl flex items-center">Sergei Kazanin</a>
-            <Link>Мои проекты</Link>
-            <Link>О бо мне</Link>
+            <Link to="/">
+              <div className="flex">
+                <HomeSvg />
+                <span className="ml-1 font-greatvibes transition-colors hover:text-indigo-500 text-2xl">
+                  Sergei Kazanin
+                </span>
+              </div>
+            </Link>
+            <Link to="/myproject">
+              <div className="flex">
+                <AcademicSvg />
+                <span className="ml-1">Мои проекты</span>
+              </div>
+            </Link>
+            <Link to="aboutme">
+              <div className="flex">
+                <AboutMeSvg />
+                <span className="ml">Обо мне</span>
+              </div>
+            </Link>
           </nav>
         </div>
       </header>
@@ -18,7 +41,8 @@ function App() {
         <div className="px-4 max-w-screen-lg mx-auto">
           <Routes>
             <Route path="/" element={<HomePage />} />
-            {/* <Route path="/favourites" element={<FavouritesPage />} /> */}
+            <Route path="/myproject" element={<MyProjects />} />
+            <Route path="/aboutme" element={<AboutMe />} />
           </Routes>
         </div>
       </main>
