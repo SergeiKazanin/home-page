8854b4b64c70cb9d46c97d919da4b8a9e114184e
SergeiKazanin <serega7667@gmail.com>
2023-03-17 17:17:19 +0700

    Added about page

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,8 +1,33 @@
-import React from 'react'
+import React from "react";
 
 export default function AboutMe() {
   document.title = "Обо мне - SergeiKazanin Home Page";
   return (
-    <div>AboutMe</div>
-  )
+    <div className="py-4">
+      <h1 className="mt-3">
+        {" "}
+        <strong>ОБО МНЕ</strong>
+      </h1>
+      <p className="mt-3">
+        Всем привет и добро пожаловать в раздел обо мне! Меня зовут Сергей
+        Казанин и я начинающий веб-разработчик.
+      </p>
+      <h1 className="mt-3 ">
+        <strong>НАВЫКИ</strong>
+      </h1>
+      <ul className="mt-3 flex flex-wrap gap-2">
+        <li className="rounded-2xl  text-white bg-indigo-600 px-2">HTML</li>
+        <li className="rounded-2xl  text-white bg-indigo-600 px-2">CSS, SCSS, Tailwind CSS, Material UI</li>
+        <li className="rounded-2xl  text-white bg-indigo-600 px-2">JavaScript</li>
+        <li className="rounded-2xl  text-white bg-indigo-600 px-2">ReactJs, Redux, Redux Toolkit, RTK Query</li>
+        <li className="rounded-2xl  text-white bg-indigo-600 px-2">NodeJs, Express</li>
+        <li className="rounded-2xl  text-white bg-indigo-600 px-2">GitHub</li>
+        <li className="rounded-2xl  text-white bg-indigo-600 px-2">Figma</li>
+        <li className="rounded-2xl  text-white bg-indigo-600 px-2">C, Arduino, PlatformIO</li>
+      </ul>
+      <p className="mt-3">
+        Самый важный навык — это умение учиться и никогда не сдаваться.
+      </p>
+    </div>
+  );
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,8 +4,8 @@ export default function HomePage() {
   document.title = "Главная - SergeiKazanin Home Page";
   return (
     <div className="py-4 text-center">
-      <img className="rounded-full m-auto w-40 h-40 shadow-xl"  src="selfie.jpg" alt="logo"></img>
-        <h1 className="">Здравствуйте! <br></br> Mеня зовут <strong>Сергей Казанин</strong>.</h1>
+      <img className="rounded-full mt-3 mx-auto w-40 h-40 shadow-xl"  src="selfie.jpg" alt="logo"></img>
+        <h1 className="mt-3">Здравствуйте! <br></br> Mеня зовут <strong>Сергей Казанин</strong>.</h1>
         <p className="mt-3">Я начинающий веб разработчик. </p>
     </div>
   );
