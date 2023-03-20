c2b2db4b4450b3bc02984ee5ba97a6dc0a166542
SergeiKazanin <serega7667@gmail.com>
2023-03-20 11:24:32 +0700

    Edit project page

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -7,13 +7,14 @@ export default function MyProjects() {
     {
       bgImg: "bg-dictionary",
       descriptions:
-        "Словарь с изспользованием API, Tailwind CSS, redux toolkit, RTK Query, dark theme, Material UI",
+        "Словарь на react с использованием API, Tailwind CSS, redux toolkit, RTK Query, dark theme, Material UI",
       linkProtect: "https://sergeikazanin.github.io/dictionary-on-react/",
       linkGitHab: "https://github.com/SergeiKazanin/dictionary-on-react",
     },
     {
       bgImg: "bg-weather",
-      descriptions: "Погода с использованием API openweathermap.org",
+      descriptions:
+        "Погода на чистом js с использованием API openweathermap.org",
       linkProtect:
         "https://sergeikazanin.github.io/weather-js-app-with-adaptive/",
       linkGitHab:
@@ -28,7 +29,7 @@ export default function MyProjects() {
     {
       bgImg: "bg-co2mert",
       descriptions: "Измеритель углекислого газа и PM 2.5 частиц в воздухе",
-      linkProtect: "https://github.com/SergeiKazanin/carbon-dioxide-meter",
+      linkProtect: "https://user-images.githubusercontent.com/105712313/226246619-9f593c82-dd07-454e-be16-b56687b1508d.jpg",
       linkGitHab: "https://github.com/SergeiKazanin/carbon-dioxide-meter",
     },
     {
@@ -45,17 +46,18 @@ export default function MyProjects() {
     },
   ];
 
-  const Card = ({project}) => {
+  const Card = ({ project }) => {
     return (
-      <div className="w-[20rem] flex flex-col overflow-hidden border dark:border-none rounded-md shadow-md transition-all hover:shadow-lg">
+      <div className="w-[20rem] flex flex-col overflow-hidden border dark:border-none rounded-2xl shadow-md">
         <div
           className={`w-full bg-cover bg-no-repeat dark:bg-slate-600 bg-center pb-[78%] ${project.bgImg}`}
         ></div>
-        <div className="p-2 flex flex-1 flex-col  items-center dark:bg-slate-600 border-t  dark:border-slate-400">
+        <div className="p-2 flex flex-1 flex-col  items-center dark:bg-slate-600 border-t  dark:border-slate-500">
           <p className="">{project.descriptions}</p>
           <div className="w-full flex flex-1 flex-col items-center justify-end">
             <a
-              className="mt-2 flex items-center justify-center rounded-md w-3/4 h-10 bg-indigo-300 transition-colors hover:bg-indigo-400"
+              className="mt-2 flex items-center justify-center rounded-3xl hover:shadow-lg shadow-md w-3/4 h-10 dark:bg-indigo-800
+                         dark:hover:bg-indigo-900 bg-indigo-300 hover:bg-indigo-400 transition-colors"
               target="_blank"
               rel="noreferrer"
               href={project.linkProtect}
@@ -64,7 +66,8 @@ export default function MyProjects() {
             </a>
 
             <a
-              className="mt-2 mb-2 flex items-center justify-center rounded-md w-3/4 h-10 bg-indigo-300 transition-colors hover:bg-indigo-400"
+              className="my-2 flex items-center justify-center rounded-3xl hover:shadow-lg shadow-md w-3/4 h-10 dark:bg-indigo-800
+                         dark:hover:bg-indigo-900 bg-indigo-300 hover:bg-indigo-400 transition-colors"
               target="_blank"
               rel="noreferrer"
               href={project.linkGitHab}
@@ -84,7 +87,7 @@ export default function MyProjects() {
       </p>
       <div className="flex flex-wrap gap-4 mt-8">
         {projects.map((project) => (
-          <Card key={project.bgImg} project ={project}/>
+          <Card key={project.bgImg} project={project} />
         ))}
       </div>
     </div>
