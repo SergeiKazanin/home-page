be39a1cf827ff2711a6f5c963887b362dfd8245c
SergeiKazanin <serega7667@gmail.com>
2023-03-26 08:33:37 +0700

    Added Skeleton loading

--- src/pages/MyProjects.js
+++ src/pages/MyProjects.js
@@ -1,55 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useGetProjectQuery } from "../store/projectApi";
+import Skeleton from "@mui/material/Skeleton";
 
 export default function MyProjects() {
   document.title = "Мои проекты - SergeiKazanin Home Page";
 
-  // let projects = [
-  //   {
-  //     bgImg: "bg-dictionary",
-  //     descriptions:
-  //       "Словарь на react с использованием API, Tailwind CSS, redux toolkit, RTK Query, dark theme, Material UI",
-  //     linkProtect: "https://sergeikazanin.github.io/dictionary-on-react/",
-  //     linkGitHab: "https://github.com/SergeiKazanin/dictionary-on-react",
-  //   },
-  //   {
-  //     bgImg: "bg-weather",
-  //     descriptions:
-  //       "Погода на чистом js с использованием API openweathermap.org",
-  //     linkProtect:
-  //       "https://sergeikazanin.github.io/weather-js-app-with-adaptive/",
-  //     linkGitHab:
-  //       "https://github.com/SergeiKazanin/weather-js-app-with-adaptive",
-  //   },
-  //   {
-  //     bgImg: "bg-snake",
-  //     descriptions: "Игра змейка на canvas и js",
-  //     linkProtect: "https://sergeikazanin.github.io/Snake/",
-  //     linkGitHab: "https://github.com/SergeiKazanin/Snake",
-  //   },
-  //   {
-  //     bgImg: "bg-co2mert",
-  //     descriptions: "Измеритель углекислого газа и PM 2.5 частиц в воздухе",
-  //     linkProtect:
-  //       "https://user-images.githubusercontent.com/105712313/226246619-9f593c82-dd07-454e-be16-b56687b1508d.jpg",
-  //     linkGitHab: "https://github.com/SergeiKazanin/carbon-dioxide-meter",
-  //   },
-  //   {
-  //     bgImg: "bg-test",
-  //     descriptions: "Тест с использованием API",
-  //     linkProtect: "https://sergeikazanin.github.io/TestingSystem-front",
-  //     linkGitHab: "https://github.com/SergeiKazanin/TestingSystem-front",
-  //   },
-  //   {
-  //     bgImg: "bg-login",
-  //     descriptions: "Авторизация",
-  //     linkProtect: "https://sergeikazanin.github.io/Login-Page-UI/",
-  //     linkGitHab: "https://github.com/SergeiKazanin/Login-Page-UI",
-  //   },
-  // ];
-
-  const Card = ({ project }) =>{ 
-    console.log(project)
+  const Card = ({ project }) => {
+    console.log(project);
     const imgArr = project.poster.asset._ref.split("-");
     const imgLink = imgArr[1] + "-" + imgArr[2] + "." + imgArr[3];
     const img = `https://cdn.sanity.io/images/94p50dfz/production/${imgLink}`;
@@ -92,7 +49,22 @@ export default function MyProjects() {
   } = useGetProjectQuery(`*[_type == "projects"]`);
 
   if (isFetching) {
-    return <h2 className="text-4xl">Loading</h2>;
+    return (
+      <div className="py-4 text-center">
+        <h1>МОИ ПРОЕКТЫ</h1>
+        <p className="mt-3">
+          Приложения разработанные для обучнения и просто по фану.
+        </p>
+        <div className="flex flex-wrap justify-center gap-4 mt-8">
+          <Skeleton variant="rounded" width={320} height={470} />
+          <Skeleton variant="rounded" width={320} height={470} />
+          <Skeleton variant="rounded" width={320} height={470} />
+          <Skeleton variant="rounded" width={320} height={470} />
+          <Skeleton variant="rounded" width={320} height={470} />
+          <Skeleton variant="rounded" width={320} height={470} />
+        </div>
+      </div>
+    );
   }
 
   if (isError) {
--- src/store/projectApi.js
+++ src/store/projectApi.js
@@ -3,11 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const projectApi = createApi({
   reducerPath: "projectApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `https://94p50dfz.api.sanity.io/v2021-10-21/data/query/production?query=*[_type == "projects"]`,
+    baseUrl: "https://94p50dfz.api.sanity.io/v2021-10-21/data/query/",
   }),
   endpoints: (builder) => ({
     getProject: builder.query({
-      query: (query) => ``,
+      query: (query) => `production?query=${query}`,
     }),
   }),
 });
