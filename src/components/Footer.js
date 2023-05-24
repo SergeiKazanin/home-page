a0ce7151a8853e1911d04a777ff5d2de4ac02483
SergeiKazanin <serega7667@gmail.com>
2023-05-24 12:00:16 +0700

    edit errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,9 +5,10 @@ export default function Footer() {
   return (
     <footer className="bg-slate-200 transition-all dark:text-white dark:bg-slate-800 shadow-inner border-t border-slate-400 text-center">
       <div className="px-4 max-w-screen-lg mx-auto py-8">
-        <h3 className="mb-3">Sergei Kazanin Home Page</h3>
+        <h3 className="mb-3 text-2xl">Sergei Kazanin Home Page</h3>
         <p>
-          {new Date().getFullYear()} Дизайн и разработка
+          {new Date().getFullYear()}
+          <span> Дизайн и разработка </span>
           <Link className="underline" to="/aboutme">
             Сергей&nbsp;Казанин
           </Link>
